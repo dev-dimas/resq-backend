40e18606ea616d634a6e83f48f183c66daf9e5c1
Dimas Maulana <dev.dimasmaulana@gmail.com>
2024-08-08 16:21:03 +0700

    refactor: turn off validation uid on subscribe and favorite

--- src/customer/customer.validation.ts
+++ src/customer/customer.validation.ts
@@ -2,18 +2,18 @@ import { ZodType, z } from 'zod';
 
 export class CustomerValidation {
   static readonly SUBSCRIBE: ZodType = z.object({
-    to: z.string().uuid(),
+    to: z.string(),
   });
 
   static readonly UNSUBSCRIBE: ZodType = z.object({
-    from: z.string().uuid(),
+    from: z.string(),
   });
 
   static readonly FAVORITE: ZodType = z.object({
-    productId: z.string().uuid(),
+    productId: z.string(),
   });
 
   static readonly UNFAVORITE: ZodType = z.object({
-    productId: z.string().uuid(),
+    productId: z.string(),
   });
 }
